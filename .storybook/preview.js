c72ccb5be0e35990628e056cf74688051dcff720
Tri Denda <tridenda.nk@gmail.com>
2023-01-19 23:55:38 +0700

    build: implement storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,46 @@
+import * as NextImage from "next/image";
+
+import "../styles/globals.css";
+
+const BREAKPOINTS_INT = {
+  xs: 375,
+  sm: 768,
+  md: 879,
+  lg: 1024,
+  xl: 1200,
+};
+
+const customViewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    console.log(val);
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ];
+  })
+);
+
+// Allow Storybook to handle Next's <Image> component
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: { viewports: customViewports },
+};
