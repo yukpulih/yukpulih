6807341c6c78847035d6ddd0a85e1ed0b3da843d
Tri Denda <tridenda.nk@gmail.com>
2023-03-04 23:15:38 +0700

    feat: removed old navbar

--- src/components/navigation/navbar-old/button/button.component.tsx
+++
@@ -1,9 +0,0 @@
-const Button = () => {
-  return (
-    <button className="md:max-w-[100px] w-full bg-green-900 rounded-lg text-gray-300 px-6 py-1 hover:outline hover:outline-2  hover:outline-offset-2 hover:outline-green-900 focus:bg-white">
-      Login
-    </button>
-  );
-};
-
-export default Button;
