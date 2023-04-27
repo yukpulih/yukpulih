ece0c4eb69ac08bb61a01fd865d9503bd43f9aea
dh-i <dhanush.code1@gmail.com>
2023-04-27 11:53:08 +0530

    feat: create hero section

---
+++ src/components/sections/hero/hero.component.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import heroImg from "public/static/hero-logo-img.png";
+import PrimaryBtn from "../../elements/buttons/primary-button.component";
+
+const Hero: React.FC = () => {
+  return (
+    <section id="hero" className="max-w-full">
+      <div className="flex flex-col-reverse md:flex-row gap-8 max-w-screen-xl px-14 mx-auto items-center justify-center text-center md:text-left py-12 md:py-24">
+        <div className="md:max-w-[50%] lg:pr-8">
+          <h1 className="text-[1.75rem] leading-10 font-serif pb-6">
+            We are a non-profit organization with mission to raise awareness of
+            porn’s negative effects and help addicts to free themselves from
+            porn addiction.
+          </h1>
+          <div className="flex gap-4 flex-col justify-items-center lg:flex-row sm:max-w-[75%] md:max-w-full mx-auto">
+            <PrimaryBtn>I&apos;m an addict</PrimaryBtn>
+            <PrimaryBtn>I want to be a volunteer</PrimaryBtn>
+          </div>
+        </div>
+        <div className="md:max-w-[50%]">
+          <Image src={heroImg} alt="hero image" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/sections/hero/hero.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Hero from "./hero.component";
+
+export default {
+  title: "sections/Hero",
+  component: Hero,
+  argTypes: {},
+} as ComponentMeta<typeof Hero>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Hero> = () => <Hero />;
+
+export const Base = Template.bind({});
