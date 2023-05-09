4a12489b18197193a16be8331723fce57139f8d6
dh-i <dhanush.code1@gmail.com>
2023-05-10 01:02:22 +0530

    fix: update spacing in pillars section cards

--- src/components/sections/pillars/card.component.tsx
+++ src/components/sections/pillars/card.component.tsx
@@ -11,7 +11,7 @@ const Card: React.FC<CardProps> = ({ heading, content, Icon }) => {
       <h3 className="font-serif font-semibold text-xl md:text-2xl leading-6 sm:leading-10 text-[#333]">
         {heading}
       </h3>
-      <p className="text-base leading-6 text-[#505050] w-[97%] mx-auto pt-1 md:pt-0">
+      <p className="text-base leading-6 text-[#505050] w-[97%] mx-auto pt-1">
         {content}
       </p>
     </div>
